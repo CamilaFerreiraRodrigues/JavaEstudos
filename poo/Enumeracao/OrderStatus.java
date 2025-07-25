49028254cc92744cc530392fd4c2d634e6acaf7f
Camila Ferreira Rodrigues <131890075+CamilaFerreiraRodrigues@users.noreply.github.com>
2025-07-25 10:46:44 -0300

    Add files via upload

---
+++ poo/Enumeracao/OrderStatus.java
@@ -0,0 +1,12 @@
+package org.exemplo2;
+
+public enum OrderStatus {
+
+    // Classe com um conjunto de valores que não muda :)
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+
+
+}
